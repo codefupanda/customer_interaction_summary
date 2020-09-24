b4a4a215889144bbbd6c7c17c7290da5d0389b31
Shashank Kulkarni <Shashank_Kulkarni@intuit.com>
2020-09-24 11:54:35 +0530

    Added data aug

--- src/models/model_configs.py
+++ src/models/model_configs.py
@@ -1,5 +1,85 @@
 model_configs = {
-    
+    "DNN": {
+        "class_name": "DNNModel",
+        "meta": {
+            "include_glove": False
+        }, 
+        "params": {
+
+        }
+    }, 
+    "DNN_Glove": {
+        "class_name": "DNNModel",
+        "meta": {
+            "include_glove": True
+        }, 
+        "params": {
+
+        }
+    },
+    "SimpleRNN": {
+        "class_name": "SimpleRNNModel",
+        "meta": {
+            "include_glove": False
+        }, 
+        "params": {
+
+        }
+    }, 
+    "SimpleRNN_Glove": {
+        "class_name": "SimpleRNNModel",
+        "meta": {
+            "include_glove": True
+        }, 
+        "params": {
+
+        }
+    },
+    "CNN": {
+        "class_name": "CNNModel",
+        "meta": {
+            "include_glove": False
+        }, 
+        "params": {
+
+        }
+    }, 
+    "CNN_Glove": {
+        "class_name": "CNNModel",
+        "meta": {
+            "include_glove": True
+        }, 
+        "params": {
+
+        }
+    },
+    "LSTM": {
+        "class_name": "LSTMModel",
+        "meta": {
+            "include_glove": False
+        }, 
+        "params": {
+
+        }
+    }, 
+    "LSTM_Glove": {
+        "class_name": "LSTMModel",
+        "meta": {
+            "include_glove": True
+        }, 
+        "params": {
+
+        }
+    },
+    "HybridModel": {
+        "class_name": "HybridModel",
+        "meta": {
+            "include_glove": False
+        }, 
+        "params": {
+
+        }
+    }, 
     "HybridModel_Glove": {
         "class_name": "LSTMModel",
         "meta": {
