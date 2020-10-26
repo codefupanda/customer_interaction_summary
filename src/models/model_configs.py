0fbd01b247edcb0fad8b3f0bb486c868f4865f18
Shashank Kulkarni <Shashank_Kulkarni@intuit.com>
2020-10-26 11:18:47 +0530

    Update model_configs.py

--- src/models/model_configs.py
+++ src/models/model_configs.py
@@ -1,58 +1,58 @@
 model_configs = {
-    "DNN": {
-        "class_name": "DNNModel",
-        "meta": {
-            "include_glove": False
-        }, 
-        "params": {
+    # "DNN": {
+    #     "class_name": "DNNModel",
+    #     "meta": {
+    #         "include_glove": False
+    #     }, 
+    #     "params": {
 
-        }
-    }, 
-    "DNN_Glove": {
-        "class_name": "DNNModel",
-        "meta": {
-            "include_glove": True
-        }, 
-        "params": {
+    #     }
+    # }, 
+    # "DNN_Glove": {
+    #     "class_name": "DNNModel",
+    #     "meta": {
+    #         "include_glove": True
+    #     }, 
+    #     "params": {
 
-        }
-    },
-    "SimpleRNN": {
-        "class_name": "SimpleRNNModel",
-        "meta": {
-            "include_glove": False
-        }, 
-        "params": {
+    #     }
+    # },
+    # # "SimpleRNN": {
+    # #     "class_name": "SimpleRNNModel",
+    # #     "meta": {
+    # #         "include_glove": False
+    # #     }, 
+    # #     "params": {
 
-        }
-    }, 
-    "SimpleRNN_Glove": {
-        "class_name": "SimpleRNNModel",
-        "meta": {
-            "include_glove": True
-        }, 
-        "params": {
+    # #     }
+    # # }, 
+    # # "SimpleRNN_Glove": {
+    # #     "class_name": "SimpleRNNModel",
+    # #     "meta": {
+    # #         "include_glove": True
+    # #     }, 
+    # #     "params": {
 
-        }
-    },
-    "CNN": {
-        "class_name": "CNNModel",
-        "meta": {
-            "include_glove": False
-        }, 
-        "params": {
+    # #     }
+    # # },
+    # "CNN": {
+    #     "class_name": "CNNModel",
+    #     "meta": {
+    #         "include_glove": False
+    #     }, 
+    #     "params": {
 
-        }
-    }, 
-    "CNN_Glove": {
-        "class_name": "CNNModel",
-        "meta": {
-            "include_glove": True
-        }, 
-        "params": {
+    #     }
+    # }, 
+    # "CNN_Glove": {
+    #     "class_name": "CNNModel",
+    #     "meta": {
+    #         "include_glove": True
+    #     }, 
+    #     "params": {
 
-        }
-    },
+    #     }
+    # },
     "LSTM": {
         "class_name": "LSTMModel",
         "meta": {
