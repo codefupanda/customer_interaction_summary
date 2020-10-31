b3a75dfb6dce4f8b7bf1486480da90434b2d3601
Shashank Kulkarni <Shashank_Kulkarni@intuit.com>
2020-10-31 19:51:50 +0530

    Update model_configs.py

--- src/models/model_configs.py
+++ src/models/model_configs.py
@@ -1,14 +1,14 @@
 model_configs = {
-    "DNN": {
-        "class_name": "DNNModel",
-        "meta": {
-            "include_glove": False
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    }, 
+    # "DNN": {
+    #     "class_name": "DNNModel",
+    #     "meta": {
+    #         "include_glove": False
+    #     }, 
+    #     "params": {
+    #         "dropout": 0.3,
+    #         "initial_learning_rate": 0.01
+    #     }
+    # }, 
     "DNN_Glove": {
         "class_name": "DNNModel",
         "meta": {
@@ -19,96 +19,96 @@ model_configs = {
             "initial_learning_rate": 0.01
         }
     },
-    "SimpleRNN": {
-        "class_name": "SimpleRNNModel",
-        "meta": {
-            "include_glove": False
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    }, 
-    "SimpleRNN_Glove": {
-        "class_name": "SimpleRNNModel",
-        "meta": {
-            "include_glove": True
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    },
-    "CNN": {
-        "class_name": "CNNModel",
-        "meta": {
-            "include_glove": False
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    }, 
-    "CNN_Glove": {
-        "class_name": "CNNModel",
-        "meta": {
-            "include_glove": True
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    },
-    "GRU": {
-        "class_name": "GRUModel",
-        "meta": {
-            "include_glove": False
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    },
-    "GRU_Glove": {
-        "class_name": "GRUModel",
-        "meta": {
-            "include_glove": True
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    },
-    "StackedGRU": {
-        "class_name": "StackedGRUModel",
-        "meta": {
-            "include_glove": False
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    },
-    "StackedGRU_Glove": {
-        "class_name": "StackedGRUModel",
-        "meta": {
-            "include_glove": True
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    },
-    "LSTM": {
-        "class_name": "LSTMModel",
-        "meta": {
-            "include_glove": False
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    }, 
+    # "SimpleRNN": {
+    #     "class_name": "SimpleRNNModel",
+    #     "meta": {
+    #         "include_glove": False
+    #     }, 
+    #     "params": {
+    #         "dropout": 0.3,
+    #         "initial_learning_rate": 0.01
+    #     }
+    # }, 
+    # "SimpleRNN_Glove": {
+    #     "class_name": "SimpleRNNModel",
+    #     "meta": {
+    #         "include_glove": True
+    #     }, 
+    #     "params": {
+    #         "dropout": 0.3,
+    #         "initial_learning_rate": 0.01
+    #     }
+    # },
+    # "CNN": {
+    #     "class_name": "CNNModel",
+    #     "meta": {
+    #         "include_glove": False
+    #     }, 
+    #     "params": {
+    #         "dropout": 0.3,
+    #         "initial_learning_rate": 0.01
+    #     }
+    # }, 
+    # "CNN_Glove": {
+    #     "class_name": "CNNModel",
+    #     "meta": {
+    #         "include_glove": True
+    #     }, 
+    #     "params": {
+    #         "dropout": 0.3,
+    #         "initial_learning_rate": 0.01
+    #     }
+    # },
+    # "GRU": {
+    #     "class_name": "GRUModel",
+    #     "meta": {
+    #         "include_glove": False
+    #     }, 
+    #     "params": {
+    #         "dropout": 0.3,
+    #         "initial_learning_rate": 0.01
+    #     }
+    # },
+    # "GRU_Glove": {
+    #     "class_name": "GRUModel",
+    #     "meta": {
+    #         "include_glove": True
+    #     }, 
+    #     "params": {
+    #         "dropout": 0.3,
+    #         "initial_learning_rate": 0.01
+    #     }
+    # },
+    # "StackedGRU": {
+    #     "class_name": "StackedGRUModel",
+    #     "meta": {
+    #         "include_glove": False
+    #     }, 
+    #     "params": {
+    #         "dropout": 0.3,
+    #         "initial_learning_rate": 0.01
+    #     }
+    # },
+    # "StackedGRU_Glove": {
+    #     "class_name": "StackedGRUModel",
+    #     "meta": {
+    #         "include_glove": True
+    #     }, 
+    #     "params": {
+    #         "dropout": 0.3,
+    #         "initial_learning_rate": 0.01
+    #     }
+    # },
+    # "LSTM": {
+    #     "class_name": "LSTMModel",
+    #     "meta": {
+    #         "include_glove": False
+    #     }, 
+    #     "params": {
+    #         "dropout": 0.3,
+    #         "initial_learning_rate": 0.01
+    #     }
+    # }, 
     "LSTM_Glove": {
         "class_name": "LSTMModel",
         "meta": {
@@ -119,16 +119,16 @@ model_configs = {
             "initial_learning_rate": 0.01
         }
     },
-    "StackedLSTM": {
-        "class_name": "StackedLSTMModel",
-        "meta": {
-            "include_glove": False
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    }, 
+    # "StackedLSTM": {
+    #     "class_name": "StackedLSTMModel",
+    #     "meta": {
+    #         "include_glove": False
+    #     }, 
+    #     "params": {
+    #         "dropout": 0.3,
+    #         "initial_learning_rate": 0.01
+    #     }
+    # }, 
     "StackedLSTM_Glove": {
         "class_name": "StackedLSTMModel",
         "meta": {
@@ -139,16 +139,16 @@ model_configs = {
             "initial_learning_rate": 0.01
         }
     },
-    "BiLSTM": {
-        "class_name": "BiLSTMModel",
-        "meta": {
-            "include_glove": False
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    }, 
+    # "BiLSTM": {
+    #     "class_name": "BiLSTMModel",
+    #     "meta": {
+    #         "include_glove": False
+    #     }, 
+    #     "params": {
+    #         "dropout": 0.3,
+    #         "initial_learning_rate": 0.01
+    #     }
+    # }, 
     "BiLSTM_Glove": {
         "class_name": "BiLSTMModel",
         "meta": {
@@ -159,16 +159,16 @@ model_configs = {
             "initial_learning_rate": 0.01
         }
     },
-   "StackedBiLSTM": {
-        "class_name": "StackedBiLSTMModel",
-        "meta": {
-            "include_glove": False
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    }, 
+#    "StackedBiLSTM": {
+#         "class_name": "StackedBiLSTMModel",
+#         "meta": {
+#             "include_glove": False
+#         }, 
+#         "params": {
+#             "dropout": 0.3,
+#             "initial_learning_rate": 0.01
+#         }
+#     }, 
     "StackedBiLSTM_Glove": {
         "class_name": "StackedBiLSTMModel",
         "meta": {
@@ -179,16 +179,16 @@ model_configs = {
             "initial_learning_rate": 0.01
         }
     },
-    "HybridModel": {
-        "class_name": "HybridModel",
-        "meta": {
-            "include_glove": False
-        }, 
-        "params": {
-            "dropout": 0.3,
-            "initial_learning_rate": 0.01
-        }
-    },
+    # "HybridModel": {
+    #     "class_name": "HybridModel",
+    #     "meta": {
+    #         "include_glove": False
+    #     }, 
+    #     "params": {
+    #         "dropout": 0.3,
+    #         "initial_learning_rate": 0.01
+    #     }
+    # },
     "HybridModel_Glove": {
         "class_name": "LSTMModel",
         "meta": {
