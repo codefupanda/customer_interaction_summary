c44a5ae66010feff110639c1f28edf88b3115b5d
Sanju <tsanju@BANL16e75a85d.local>
2020-10-31 17:03:02 +0530

    Adding GRU

--- src/models/model_configs.py
+++ src/models/model_configs.py
@@ -159,4 +159,14 @@ model_configs = {
             "initial_learning_rate": 0.01
         }
     },
+    "GRU": {
+        "class_name": "GRUModel",
+        "meta": {
+            "include_glove": False
+        }, 
+        "params": {
+            "dropout": 0.2,
+            "initial_learning_rate": 0.01
+        }
+    }, 
 }
