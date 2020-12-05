10f47efff0233a1a7b41e659ed06bf3b3917537c
Shashank Kulkarni <Shashank_Kulkarni@intuit.com>
2020-12-05 11:02:50 +0530

    Update to run all the models

--- src/models/models.py
+++ src/models/models.py
@@ -16,7 +16,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras import Model
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, Flatten, Dense, Softmax, LSTM, GRU, SimpleRNN, Conv1D, MaxPooling1D, GlobalMaxPooling1D, Bidirectional, Dropout
-
+import tensorflow_hub as hub
 # from keras_self_attention import SeqSelfAttention
 
 # Hyperparameter tunning 
@@ -44,17 +44,29 @@ class DNNModel(HyperModel):
         initial_learning_rate = self.initial_learning_rate if self.initial_learning_rate else hp.Float('initial_learning_rate', min_value=0.01, max_value=0.1, default=0.01, step=0.04)
         dropout = self.dropout if self.dropout else hp.Float('dropout', min_value=0.2, max_value=0.4, default=0.3, step=0.1)
         recurrent_dropout = 0 # self.recurrent_dropout if self.recurrent_dropout else hp.Float('recurrent_dropout', min_value=0.0, max_value=0.4, default=0.3, step=0.1)
-
-        model = Sequential()
-        if embedding_matrix is not None:
-            print("Including glove")
-            model.add(Embedding(max_words, output_dim=output_dim, input_length=pad_sequences_maxlen, weights=[embedding_matrix], trainable=False))
+    
+        if embedding_matrix is not None and embedding_matrix == 'bert':
+            input_word_ids = tf.keras.Input(shape=(pad_sequences_maxlen,), dtype=tf.int32)
+            input_mask = tf.keras.Input(shape=(pad_sequences_maxlen,), dtype=tf.int32)
+            segment_ids = tf.keras.Input(shape=(pad_sequences_maxlen,), dtype=tf.int32)
+            bert_layer = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/1", trainable=False)
+            _, sequence_output = bert_layer(inputs=[input_word_ids, input_mask, segment_ids])
+            clf_output = sequence_output[:, 0, :]
+            # out = Dropout(dropout)(sequence_output)
+            out = Dense(128, activation='relu')(sequence_output)
+            out = Dense(number_of_classes + 1,  activation='softmax')(out)
+            model = Model(inputs=[input_word_ids, input_mask, segment_ids], outputs=out)
         else:
-            model.add(Embedding(max_words, output_dim=output_dim, input_length=pad_sequences_maxlen))
-        model.add(Flatten())
-        model.add(Dropout(dropout))
-        model.add(Dense(128, activation='relu'))
-        model.add(Dense(number_of_classes + 1,  activation='softmax'))
+            model = Sequential()
+            if embedding_matrix is not None:
+                print("Including glove")
+                model.add(Embedding(max_words, output_dim=output_dim, input_length=pad_sequences_maxlen, weights=[embedding_matrix], trainable=False))
+            else:
+                model.add(Embedding(max_words, output_dim=output_dim, input_length=pad_sequences_maxlen))
+            model.add(Flatten())
+            model.add(Dropout(dropout))
+            model.add(Dense(128, activation='relu'))
+            model.add(Dense(number_of_classes + 1,  activation='softmax'))
         
         lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
             initial_learning_rate=initial_learning_rate,
@@ -63,6 +75,7 @@ class DNNModel(HyperModel):
         opt = Adam(learning_rate=lr_schedule, decay=initial_learning_rate/20)
         # opt = tf.train.experimental.enable_mixed_precision_graph_rewrite(opt)
         model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy', f1_m])
+        print(model.summary())
         return model
 
 
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -21,13 +21,16 @@ from tensorflow.keras.preprocessing.text import one_hot, Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, Flatten, Dense, Softmax
-
+import tensorflow_hub as hub
+from tensorflow.keras import layers
+import bert
 import models
 from model_configs import model_configs
 # from models import Net
 
 import kerastuner
 from kerastuner.tuners import RandomSearch
+
 
 @click.command()
 @click.option('--input_filepath', default='data/processed', type=click.Path())
@@ -50,27 +53,44 @@ def main(input_filepath, output_filepath, pad_sequences_maxlen, max_words, epoch
     logger.info('--epochs ' + str(epochs))
     logger.info('--batch_size ' + str(batch_size))
 
-    isear = pd.read_csv(input_filepath + '/isear_train.csv', sep='|', error_bad_lines=False, usecols=['SIT', 'EMOT'])
+    isear = pd.read_csv(input_filepath + '/isear_train.csv',
+                        sep='|', error_bad_lines=False, usecols=['SIT', 'EMOT'])
     isear['SIT'] = isear['SIT'].astype(str)
-    isear_test = pd.read_csv(input_filepath + '/isear_test.csv', sep='|', error_bad_lines=False, usecols=['SIT', 'EMOT'])
-    sit_train, sit_test, emot_train, emot_test = train_test_split(isear['SIT'], isear['EMOT'], test_size=0.1)
-    x_test, y_test = isear_test['SIT'], isear_test['EMOT']
+    isear_test = pd.read_csv(input_filepath + '/isear_test.csv',
+                             sep='|', error_bad_lines=False, usecols=['SIT', 'EMOT'])
+    sit_train, sit_test, emot_train, emot_test = train_test_split(
+        isear['SIT'], isear['EMOT'], test_size=0.1)
+    x_test_original, y_test = isear_test['SIT'], isear_test['EMOT']
     number_of_classes = len(isear.EMOT.unique())
 
     tokenizer = Tokenizer(num_words=max_words)
     tokenizer.fit_on_texts(sit_train)
     sequences_train = tokenizer.texts_to_sequences(sit_train)
     sequences_test = tokenizer.texts_to_sequences(sit_test)
-    x_test = tokenizer.texts_to_sequences(x_test)
+    x_test = tokenizer.texts_to_sequences(x_test_original)
 
     word_index = tokenizer.word_index
     print('Found %s unique tokens.' % len(word_index))
 
-    sequences_train = pad_sequences(sequences_train, maxlen=pad_sequences_maxlen, padding='post')
-    sequences_test = pad_sequences(sequences_test, maxlen=pad_sequences_maxlen, padding='post')
+    sequences_train = pad_sequences(
+        sequences_train, maxlen=pad_sequences_maxlen, padding='post')
+    sequences_test = pad_sequences(
+        sequences_test, maxlen=pad_sequences_maxlen, padding='post')
     x_test = pad_sequences(x_test, maxlen=pad_sequences_maxlen, padding='post')
-    embedding_matrix_glove = get_embedding_matrix(max_words, word_index, output_dim)
-    
+    embedding_matrix_glove = get_embedding_matrix(
+        max_words, word_index, output_dim)
+
+    ##### BERT #####
+    FullTokenizer = bert.bert_tokenization.FullTokenizer
+    bert_layer = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/1", trainable=False)
+    vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
+    do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
+    bert_tokenizer = FullTokenizer(vocab_file, do_lower_case)
+    bert_sequences_train = bert_encode(sit_train, bert_tokenizer, pad_sequences_maxlen)
+    bert_sequences_test = bert_encode(sit_test, bert_tokenizer, pad_sequences_maxlen)
+    bert_x_test = bert_encode(x_test_original, bert_tokenizer, pad_sequences_maxlen)
+    ##### BERT ENDS #####
+
     reports = {}
     for model_config in model_configs:
         print('Training the model: ' + model_config)
@@ -80,20 +100,32 @@ def main(input_filepath, output_filepath, pad_sequences_maxlen, max_words, epoch
         params['max_words'] = max_words
         params['number_of_classes'] = number_of_classes
         params['embedding_matrix'] = embedding_matrix_glove if model_configs[model_config]['meta']['include_glove'] else None
-        model, hyperparameters = train_models(sequences_train, sequences_test, emot_train, emot_test, class_name, epochs, batch_size, params)
-        y_pred = model.predict(x_test)
+        params['embedding_matrix'] = 'bert' if model_configs[model_config]['meta']['include_bert'] else None
+
+        if params['embedding_matrix'] == 'bert':
+            print("ghjgj - " + str(len(bert_sequences_train[0])) + str(len(bert_sequences_train[1])))
+            model, hyperparameters = train_models(
+                bert_sequences_train, bert_sequences_test, emot_train, emot_test, class_name, epochs, batch_size, params)
+            y_pred = model.predict(bert_x_test)
+        else:
+            model, hyperparameters = train_models(
+                sequences_train, sequences_test, emot_train, emot_test, class_name, epochs, batch_size, params)
+            y_pred = model.predict(x_test)
         y_pred = y_pred.argmax(axis=-1)
-        model_scores = pd.DataFrame(classification_report(y_pred, y_test, output_dict=True))
+        model_scores = pd.DataFrame(
+            classification_report(y_pred, y_test, output_dict=True))
         model.save(output_filepath + model_config)
         with open(output_filepath + model_config + "_hyperparameters.json", 'w') as file:
-            file.write(json.dumps(hyperparameters.values)) # use `json.loads` to do the reverse
+            # use `json.loads` to do the reverse
+            file.write(json.dumps(hyperparameters.values))
         reports[model_config] = model_scores
     final_report = pd.concat(reports.values(), keys=reports.keys())
     final_report.to_csv(output_filepath + "/final_report.csv")
     print(final_report)
 
+
 def train_models(x_train, x_test, y_train, y_test, model_name, epochs, batch_size, params):
-    ## Get the class object from the models file and create instance
+    # Get the class object from the models file and create instance
     model = getattr(models, model_name)(**params)
     tuner = RandomSearch(
         model,
@@ -105,7 +137,8 @@ def train_models(x_train, x_test, y_train, y_test, model_name, epochs, batch_siz
         distribution_strategy=tf.distribute.MirroredStrategy()
     )
     tuner.search_space_summary()
-    tuner.search(x_train, to_categorical(y_train), epochs=epochs, validation_data=(x_test, to_categorical(y_test)))
+    tuner.search(x_train, to_categorical(y_train), epochs=epochs,
+                 validation_data=(x_test, to_categorical(y_test)))
     return tuner.get_best_models(num_models=1)[0], tuner.oracle.get_best_trials(num_trials=1)[0].hyperparameters
 
 
@@ -131,6 +164,29 @@ def get_embedding_matrix(max_words, word_index, output_dim=50):
         if embedding_vector is not None:
             embedding_matrix[i] = embedding_vector
     return embedding_matrix
+
+
+def bert_encode(texts, tokenizer, max_len=1000):
+    all_tokens = []
+    all_masks = []
+    all_segments = []
+
+    for text in texts:
+        text = tokenizer.tokenize(text)
+            
+        text = text[:max_len-2]
+        input_sequence = ["[CLS]"] + text + ["[SEP]"]
+        pad_len = max_len - len(input_sequence)
+        
+        tokens = tokenizer.convert_tokens_to_ids(input_sequence) + [0] * pad_len
+        pad_masks = [1] * len(input_sequence) + [0] * pad_len
+        segment_ids = [0] * max_len
+        
+        all_tokens.append(tokens)
+        all_masks.append(pad_masks)
+        all_segments.append(segment_ids)
+
+    return np.array(all_tokens), np.array(all_masks), np.array(all_segments)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
